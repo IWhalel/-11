d5796c98dc99e5ef3bcf3a319fe96ce70a8ec0cc
IWhalel <rjeofyd1021@gmail.com>
2024-10-03 19:06:56 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://mediterm.netlify.app/' }}
+    />
+  );
+}
